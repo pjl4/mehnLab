36c67c303198c06240a55bdd18661a85705a3ab3
pjl4 <pj.lutz17@gmail.com>
2020-02-06 13:10:10 -0500

    formatted recipe object and created a new view

--- controllers/recipeController.js
+++ controllers/recipeController.js
@@ -4,11 +4,35 @@ const Recipe = require('../db/models/recipeModel');
 
 const router = express.Router();
 
+//read
 router.get('/', (req, res) => {
-	Recipe.find({}).then(recipes => {
-		res.render ('index',{recipes})
-
-	}).catch (console.error);
+	Recipe.find({})
+		.then((recipes) => {
+			res.render('index', { recipes });
+		})
+		.catch(console.error);
 });
+
+//create
+router.get('/new', (req, res) => {
+	res.render('new');
+});
+
+router.post('/new', (req, res) => {
+	console.log(req.body);
+	let ingredients = req.body.ingredients.split(',');
+	const recipeObject = {
+		title: req.body.title,
+		description: req.body.description,
+		instructions: req.body.instructions,
+		ingredients
+	};
+	console.log(recipeObject);
+});
+//view one
+
+//update
+
+//delete
 
 module.exports = router;
