4a82c47cad0bc400e7ed4e6649c5da1bf4d3f46d
pjl4 <pj.lutz17@gmail.com>
2020-02-06 11:30:23 -0500

    Added recipe schema

---
+++ db/models/recipeModel.js
@@ -0,0 +1,24 @@
+const mongoose = require('../connection');
+
+const RecipeSchema = new mongoose.Schema({
+	title: {
+		type: String,
+		required: true
+	},
+	description: {
+		type: String,
+		required: true
+	},
+	instructions: {
+		type: String,
+		required: true
+	},
+	ingredients: {
+		type: Array,
+		required: true
+	}
+});
+
+const Recipe = mongoose.model('Recipe', RecipeSchema);
+
+module.exports = RecipeSchema;
